1b3bfe12e6c82e6c7c2cbbfad4bbacfa2d96b330
zerobikappa <zerobikappa@gmail.com>
2022-02-02 17:10:53 +0800

    fix .remove-appdir-git.sh

--- .remove-appdir-git.sh
+++ .remove-appdir-git.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# you can run this script to remove the git project related files. It is not necessary to bundle these files into .appimage package.
+# do not move this .sh files to other location because it detects $HERE directory and deletes git files under $HERE directory.
+
 SELF=$(readlink -f "$0")
 HERE=${SELF%/*}
 
 echo "remove git related files from AppDir"
-rm -rfv "$HERE/.git*"
-rm -rfv "$HERE/README.md"
-rm -rfv "$HERE/readme-todo"
-rm -rfv "$HERE/usr/share/drive_d/savedata/.git*"
-rm -rfv "$HERE/.remove-appdir-git.sh"
+rm -rfv "$HERE"/.git*
+rm -rfv "$HERE"/README.md
+rm -rfv "$HERE"/readme-todo
+rm -rfv "$HERE"/usr/share/drive_d/savedata/.git*
+rm -rfv "$HERE"/.remove-appdir-git.sh
