2377189d65bea1df7bdc64022f66f96c5121c762
zerobikappa <zerobikappa@gmail.com>
2022-02-16 06:21:03 +0800

    update

--- .remove-appdir-git.sh
+++ .remove-appdir-git.sh
@@ -10,5 +10,8 @@ echo "remove git related files from AppDir"
 rm -rfv "$HERE"/.git*
 rm -rfv "$HERE"/README.md
 rm -rfv "$HERE"/readme-todo
-rm -rfv "$HERE"/usr/share/drive_d/savedata/.git*
+rm -rfv "$HERE"/usr/share/drive_d/myapp_savedata/.git*
+rm -rfv "$HERE"/usr/share/drive_d/myapp_walkthrough/.git*
+rm -rfv "$HERE"/usr/share/drive_d/myapp_fonts/.git*
+rm -rfv "$HERE"/usr/share/drive_d/myapp_patch_reg/.git*
 rm -rfv "$HERE"/.remove-appdir-git.sh
