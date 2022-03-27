948221a86d0bc8c1dc69086310a0e061a29a753c
zerobikappa <zerobikappa@gmail.com>
2022-03-28 05:02:45 +0800

    update

--- .remove-appdir-git.sh
+++ .remove-appdir-git.sh
@@ -20,6 +20,7 @@ echo "remove git related files from AppDir"
 find "$HERE" -name ".git*" -exec rm -rfv "{}" \;
 rm -rfv "$HERE"/usr/bin/comment.myapp*.backup
 rm -rfv "$HERE"/backup/screenshot.backup
+rm -rfv "$HERE"/backup/unionfs-fuse_1.0-1ubuntu2_amd64.deb
 rm -rfv "$HERE"/README.zh.md
 rm -rfv "$HERE"/README.md
 rm -rfv "$HERE"/readme-todo
